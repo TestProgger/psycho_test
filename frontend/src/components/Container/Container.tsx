d79f380826a53d273b03bf26b648c156f425dd3b
TestProgger <gamersteam_dubki@mail.ru>
2023-11-12 23:39:43 +0300

    Initial configuration

---
+++ frontend/src/components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react';
+import './Container.scss';
+
+
+export const Container: FC = () => {
+    return (
+        <div className="block-container">
+
+        </div>
+    )
+}
---
+++ frontend/src/pages/Main/Main.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react'
+
+
+export const MainPage : FC = () => {
+    return (
+        <div className="main-page">
+            <div className="container">
+                <div className="text">
+                    ПСИХОЛОГИЧЕСКИЕ ТЕСТЫ
+                </div>
+            </div>
+        </div>
+    )
+}
